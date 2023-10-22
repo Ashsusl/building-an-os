8c04f02eaceef6c4d242273e0ea32c75db8f7cc1
Ashish Surkheti <75275365+Ashsusl@users.noreply.github.com>
2023-10-21 18:59:17 -0700

    Added some extra functionalities and added necessary commands in the read me file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # building-an-os
 Trying to build an OS as a class project
+Also here are the necessary commands to build the environment and emulate the OS in qemu
+docker build buildenv -t myos-buildenv
+docker run --rm -it -v "%cd%":/root/env myos-buildenv
+make build-x86_64
+exit
+qemu-system-x86_64 -cdrom dist/x86_64/kernel.iso
--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -3,10 +3,7 @@
 void kernel_main()
 {
     print_clear();
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_set_color(PRINT_COLOR_BLUE, PRINT_COLOR_WHITE);
     print_str("Welcome to our 64 bit kernel!");
-    print_str("\nNow we will create interfaces to \ncreate a file, \nmove a file, \nand delete that file.");
-    int num = 42;
-    print_str("The answer is: ");
-    print_int(num);
+    print_str("\nNow we will create interfaces to \ncreate a file, \nmove a file, \nand delete that file.\n");
 }
